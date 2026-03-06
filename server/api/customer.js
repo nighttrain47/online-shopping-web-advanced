a6ab46702400c82405a9fb3c69a937323b278f32
nighttrain47 <thienbao2256@gmail.com>
2026-03-06 07:37:59 +0700

    feat: Implement customer signup and activation APIs, and consolidate public product and category endpoints.

--- server/api/customer.js
+++ server/api/customer.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+// utils
+const CryptoUtil = require('../utils/CryptoUtil');
+const EmailUtil = require('../utils/EmailUtil');
 // daos
 const CategoryDAO = require('../models/CategoryDAO');
 const ProductDAO = require('../models/ProductDAO');
+const CustomerDAO = require('../models/CustomerDAO');
 
 // ==================== CATEGORY APIs ====================
 
@@ -46,6 +50,44 @@ router.get('/products/:id', async function (req, res) {
     const _id = req.params.id;
     const product = await ProductDAO.selectById(_id);
     res.json(product);
+});
+
+// ==================== CUSTOMER APIs ====================
+
+// POST - Đăng ký tài khoản
+router.post('/signup', async function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const name = req.body.name;
+    const phone = req.body.phone;
+    const email = req.body.email;
+    const dbCust = await CustomerDAO.selectByUsernameOrEmail(username, email);
+    if (dbCust) {
+        res.json({ success: false, message: 'Exists username or email' });
+    } else {
+        const now = new Date().getTime(); // milliseconds
+        const token = CryptoUtil.md5(now.toString());
+        const newCust = { username: username, password: password, name: name, phone: phone, email: email, active: 0, token: token };
+        const result = await CustomerDAO.insert(newCust);
+        if (result) {
+            const send = await EmailUtil.send(email, result._id, token);
+            if (send) {
+                res.json({ success: true, message: 'Please check email' });
+            } else {
+                res.json({ success: false, message: 'Email failure' });
+            }
+        } else {
+            res.json({ success: false, message: 'Insert failure' });
+        }
+    }
+});
+
+// POST - Kích hoạt tài khoản
+router.post('/active', async function (req, res) {
+    const _id = req.body.id;
+    const token = req.body.token;
+    const result = await CustomerDAO.active(_id, token, 1);
+    res.json(result);
 });
 
 module.exports = router;
